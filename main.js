53a5f6dd6aecda36a582dc26a80e96d24279423d
Adrian030304 <adrianromanenco1@gmail.com>
2023-12-16 22:51:01 +0200

    created the colorign function

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let container = document.querySelector(".container")
-let grid;
+let grid ;
+for(const child of container.children){
+    child.addEventListener("mouseover", () => {child.style.backgroundColor = "orange";});
 
+}
 for(let i = 0; i<16;i++){
     for(let j = 0; j<16;j++){
         grid = document.createElement("div");
@@ -9,3 +12,5 @@ for(let i = 0; i<16;i++){
     
     
 }
+
+
