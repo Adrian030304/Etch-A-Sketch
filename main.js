f14616e40f712b29e7fe2d052531fc371faf63a0
Adrian030304 <adrianromanenco1@gmail.com>
2023-12-16 22:14:15 +0200

    created  the grid style divs

---
+++ main.js
@@ -0,0 +1,11 @@
+let container = document.querySelector(".container")
+let grid;
+
+for(let i = 0; i<16;i++){
+    for(let j = 0; j<16;j++){
+        grid = document.createElement("div");
+        container.appendChild(grid)
+    }
+    
+    
+}
