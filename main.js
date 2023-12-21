6c560b950d674ed563e2815bc8aaa7a38d83fdf5
Adrian030304 <adrianromanenco1@gmail.com>
2023-12-21 23:22:01 +0200

    adjusting the main function regarding the grid?

--- main.js
+++ main.js
@@ -3,26 +3,20 @@ let container = document.querySelector(".container")
 let grid ;
 let btn = document.querySelector("button");
 let btg = document.querySelector("#btnG")
-NodeList.prototype.forEach = Array.prototype.forEach;
 
+/*
 function sketchPad(nr){
+*/
 
-for(let i = 0; i<nr;i++){
-    for(let j = 0; j<nr;j++){
-        grid = document.createElement("div");
-        container.appendChild(grid)
-    }
-    
-    
+for(let i = 0; i< 36*36; i++){
+    grid = document.createElement("div")
+    container.appendChild(grid)
 }
 
-for(const child of container.children){
-    child.addEventListener("mouseover", () => {child.style.backgroundColor = "orange";});
-    btn.addEventListener("click",() => {child.style.backgroundColor = "";});
-}
 
-}
+/*
 btg.addEventListener("mouse",function(){
-    nr = prompt("Input the size of the sketch pad");
-    sketchPad(nr)
+    
+    sketchPad(5);
 })
+*/
