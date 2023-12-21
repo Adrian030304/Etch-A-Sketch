c02bd5a3916e648994d004840d85494644708e61
Adrian030304 <adrianromanenco1@gmail.com>
2023-12-21 23:39:36 +0200

    refactored functionality

--- main.js
+++ main.js
@@ -4,19 +4,16 @@ let grid ;
 let btn = document.querySelector("button");
 let btg = document.querySelector("#btnG")
 
-/*
-function sketchPad(nr){
-*/
 
-for(let i = 0; i< 36*36; i++){
+function sketchPad(nr){
+
+
+for(let i = 0; i< nr*nr; i++){
     grid = document.createElement("div")
     container.appendChild(grid)
 }
+}
 
-
-/*
-btg.addEventListener("mouse",function(){
-    
-    sketchPad(5);
+btg.addEventListener("click",function(){
+    sketchPad(36);
 })
-*/
