b952791c63dfe8da97a21b4225196682f6f389c6
Adrian030304 <adrianromanenco1@gmail.com>
2023-12-22 11:28:58 +0200

    created conditions for grid creation

--- main.js
+++ main.js
@@ -13,9 +13,23 @@ function sketchPad(size){
         container.appendChild(grid)
         
     }
+    /*
+    for(let i = 0; i<size;i++){
+        grid = document.createElement("div")
+        container.appendChild(grid)
+        for(let j = 0; j<size;j++){
+            grid = document.createElement("div")
+            container.appendChild(grid)
+        }
+    }
+   */
 }
 
 btg.addEventListener("click",function(){
     const size = prompt("Enter the number of squares per side (maximum: 100):")
-    sketchPad(size)
+    if(size > 0 && size <=100){
+    sketchPad(size);
+    }else{
+        alert("Invalid input . Please try again ! ")
+    }
 })
